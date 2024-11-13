32bfeaa44cac87c7622e7ed58a5a218c830ca575
Daniel MONTEIRO <101821091+DanielDSM23@users.noreply.github.com>
2024-11-13 22:33:07 +0100

    add: jwt token verfication

--- api-gateway.js
+++ api-gateway.js
@@ -1,9 +1,29 @@
 require('dotenv').config();
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const jwt = require('jsonwebtoken');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
+const SECRET_KEY = process.env.SECRET_KEY || 'secret';
+
+
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+      if (err) {
+        return res.status(403).json({ message: 'Token invalide' });
+      }
+      req.user = user;
+      next();
+    });
+  } else {
+    return res.status(401).json({ message: 'Token manquant' });
+  }
+};
+
 
 
 app.use((req, res, next) => {
@@ -18,8 +38,25 @@ app.use('/auth', createProxyMiddleware({
   },
 }));
 
-
-
+/*app.use('/command', verifyJWT, createProxyMiddleware({
+  target: process.env.PROTECTED_SERVICE_URL,
+  changeOrigin: true,
+  pathRewrite: {
+    '^/': '',
+  },
+  onProxyReq: (proxyReq, req) => {
+    if (req.user) {
+      proxyReq.setHeader('X-User-Id', req.user.id);
+      proxyReq.setHeader('X-User-Role', req.user.role);
+    }
+  },
+}));*/
+app.get('/test-jwt', verifyJWT, (req, res) => {
+  res.json({
+    message: 'Le JWT est valide',
+    user: req.user,
+  });
+});
 
 
 app.listen(PORT, () => {
--- users-service/app.js
+++ users-service/app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const port = process.env.PORT || '3000';
+const port = process.env.PORT;
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUI = require('swagger-ui-express');
 const apiRouter = require('./routes');
