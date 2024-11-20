2043722a73e21683adcebc3f2ebcabd39a657a5a
KUCA Brice <kuca.b@sfeir.com>
2024-11-20 13:47:08 +0100

    feat: update get cart

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -9,28 +9,27 @@ const PRODUCT_SERVICE_URL = process.env.PRODUCT_SERVICE_URL;
 const USER_SERVICE_URL = process.env.USER_SERVICE_URL;
 
 router.get('/', async (req, res) => {
-    try {
-      const cartResponse = await axios.get(`${process.env.CART_SERVICE_URL}api/cart/${req.user.userId}`);
-      res.json(cartResponse.data);
-      //à décommenter quand il y aura un service de produits
-      // const cart = cartResponse.data;
-  
-      // const productDetails = await Promise.all(
-      //   cart.items.map(item =>
-      //     axios.get(`${PRODUCT_SERVICE_URL}/products/${item.productId}`).then(res => res.data)
-      //   )
-      // );
-      // cart.items = cart.items.map((item, index) => ({
-      //   ...item,
-      //   productDetails: productDetails[index]
-      // }));
-  
-      // res.status(200).json(cart);
-      } catch (error) {
-        console.error(`[API Gateway] Error fetching cart: ${error.message}`);
-        res.status(500).json({ message: 'Erreur lors de la récupération du panier' });
-      }
-  });
+  try {
+    const cartResponse = await axios.get(`${process.env.CART_SERVICE_URL}api/cart/${req.user.userId}`);
+    const cart = cartResponse.data;
+    console.log(`coucou`);
+    const productDetails = await Promise.all(
+      cart.items.map(item =>
+        axios.get(`${PRODUCT_SERVICE_URL}products/${item.productId}`).then(res => res.data)
+      )
+    );
+    console.log("coucou 2", productDetails);
+    cart.items = cart.items.map((item, index) => ({
+      ...item,
+      productDetails: productDetails[index]
+    }));
+    console.log("coucou 3", cart);
+    res.send(cart);
+    } catch (error) {
+      console.error(`[API Gateway] Error fetching cart: ${error.message}`);
+      res.status(500).json({ message: 'Erreur lors de la récupération du panier' });
+    }
+});
   
   router.delete('/remove', createProxyMiddleware({
     target: process.env.CART_SERVICE_URL,
