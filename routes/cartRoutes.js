6d2741a5e4f1ce75d3dff95d6ab988596c8d2302
KUCA Brice <kuca.b@sfeir.com>
2024-11-20 13:49:31 +0100

    feat: delete console.log

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -6,24 +6,20 @@ const app = express();
 const router = express.Router();
 const CART_SERVICE_URL = process.env.CART_SERVICE_URL;
 const PRODUCT_SERVICE_URL = process.env.PRODUCT_SERVICE_URL;
-const USER_SERVICE_URL = process.env.USER_SERVICE_URL;
 
 router.get('/', async (req, res) => {
   try {
-    const cartResponse = await axios.get(`${process.env.CART_SERVICE_URL}api/cart/${req.user.userId}`);
+    const cartResponse = await axios.get(`${CART_SERVICE_URL}api/cart/${req.user.userId}`);
     const cart = cartResponse.data;
-    console.log(`coucou`);
     const productDetails = await Promise.all(
       cart.items.map(item =>
         axios.get(`${PRODUCT_SERVICE_URL}products/${item.productId}`).then(res => res.data)
       )
     );
-    console.log("coucou 2", productDetails);
     cart.items = cart.items.map((item, index) => ({
       ...item,
       productDetails: productDetails[index]
     }));
-    console.log("coucou 3", cart);
     res.send(cart);
     } catch (error) {
       console.error(`[API Gateway] Error fetching cart: ${error.message}`);
@@ -32,7 +28,7 @@ router.get('/', async (req, res) => {
 });
   
   router.delete('/remove', createProxyMiddleware({
-    target: process.env.CART_SERVICE_URL,
+    target: CART_SERVICE_URL,
     pathRewrite: {
       '^/remove': '/api/cart/remove',
     },
@@ -52,7 +48,7 @@ router.get('/', async (req, res) => {
   }));
   
   router.put('/update', createProxyMiddleware({
-    target: process.env.CART_SERVICE_URL,
+    target: CART_SERVICE_URL,
     pathRewrite: {
       '^/update': '/api/cart/update',
     },
@@ -66,7 +62,7 @@ router.get('/', async (req, res) => {
   }));
   
   router.post('/add', createProxyMiddleware({
-    target: process.env.CART_SERVICE_URL,
+    target: CART_SERVICE_URL,
     pathRewrite: {
       '^/add': '/api/cart/add',
     },
