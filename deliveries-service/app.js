97c7c8a37a6731fbf022f6880f5840f291fe5477
Daniel MONTEIRO <101821091+DanielDSM23@users.noreply.github.com>
2024-11-21 15:34:52 +0100

    resolve docker bug

--- deliveries-service/app.js
+++ deliveries-service/app.js
@@ -7,7 +7,7 @@ const deliveryRoutes = require('./routes/deliveryRoutes');
 const app = express();
 app.use(express.json());
 
-mongoose.connect(process.env.DATABASE_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.error('MongoDB connection error:', error));
 
